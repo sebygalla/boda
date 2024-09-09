7e6d87aeb358b7805fe04a6729c0e1aee899ef63
sebygalla <sebygalla@hotmail.com>
2024-09-08 22:38:52 -0300

    mediaquerys

--- js/countdown.js
+++ js/countdown.js
@@ -8,19 +8,31 @@
         minutes: 30, // Default is 0 [0-59] integer
         seconds: 0, // Default is 0 [0-59] integer
         words: { //words displayed into the countdown
-            days: { singular: 'Dia', plural: 'Dias' },
-            hours: { singular: 'Hora', plural: 'Horas' },
-            minutes: { singular: 'Minuto', plural: 'Minutos' },
-            seconds: { singular: 'Segundo', plural: 'Segundos' }
+            days: {
+                singular: 'Dia',
+                plural: 'Dias'
+            },
+            hours: {
+                singular: 'Hora',
+                plural: 'Horas'
+            },
+            minutes: {
+                singular: 'Minuto',
+                plural: 'Minutos'
+            },
+            seconds: {
+                singular: 'Segundo',
+                plural: 'Segundos'
+            }
         },
         plural: true, //use plurals
         inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
         inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
         // in case of inline set to false
         enableUtc: false, //Use UTC or not - default : false
-        onEnd: function() { 
+        onEnd: function () {
             document.getElementById('portada').classList.add('oculta');
-            return; 
+            return;
         }, //Callback on countdown end, put your own function here
 
 
@@ -30,11 +42,19 @@
         wordClass: 'simply-word', // word css class
         zeroPad: false,
         countUp: false
-});
+    });
 
-// Also, you can init with already existing Javascript Object.
-let myElement = document.querySelector('.my-countdown');
-simplyCountdown(myElement, { /* options */ });
+    // Also, you can init with already existing Javascript Object.
+    let myElement = document.querySelector('.my-countdown');
+    simplyCountdown(myElement, {
+        /* options */ });
 
-let multipleElements = document.querySelectorAll('.my-countdown');
-simplyCountdown(multipleElements, { /* options */ });
+    let multipleElements = document.querySelectorAll('.my-countdown');
+    simplyCountdown(multipleElements, {
+        /* options */ });
+
+
+    // VENTANA MODAL ( REGALO )
+
+
+
